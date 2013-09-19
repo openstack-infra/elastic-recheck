1004197193f5f1f2c4a6a4d327a39c91bddabf90
Joe Gordon <joe.gordon0@gmail.com>
2013-09-19 14:57:41 -0700

    Make Flake8 go Green

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
 #! /usr/bin/env python
 
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
 # The configuration file should look like:
 """
 [ircbot]
@@ -26,12 +41,12 @@ openstack-qa:
 import ConfigParser
 import daemon
 import irc.bot
+import logging
 import os
 import sys
 import threading
 import time
 import yaml
-import logging
 
 try:
     import daemon.pidlockfile
@@ -117,8 +132,8 @@ class RecheckWatch(threading.Thread):
 
     def run(self):
         # Import here because it needs to happen after daemonization
-        from elasticRecheck import Stream
         from elasticRecheck import Classifier
+        from elasticRecheck import Stream
         classifier = Classifier(self.queries)
         stream = Stream(self.username, self.host, self.key)
         while True:
