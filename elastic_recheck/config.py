69b588cb2b3847754f01f350d7aa9f4c2b476801
Clark Boylan <clark.boylan@gmail.com>
2017-10-27 10:10:19 -0700

    Balance parens in all fails query
    Updating uncategorized failures is currently failing on a query parse
    error in elasticsearch. This appears to be due to unbalanced parens in
    the new all fails query. Rebalance the parens by removing the extra
    leading paren.
    
    Change-Id: I05626c563a9a053e396782c54dae4c6fa7d6e269

--- elastic_recheck/config.py
+++ elastic_recheck/config.py
@@ -50,7 +50,7 @@ INCLUDED_PROJECTS_REGEX = "(^openstack/|devstack|grenade)"
 # TODO(dmsimard): Revisit this query once Zuul v2 is no longer supported
 # Let's value legibility over pep8 line width here...
 ALL_FAILS_QUERY = (
-    '((filename:"job-output.txt" AND message:"POST-RUN END" AND message:"project-config/playbooks/base/post-ssh")'  # flake8: noqa
+    '(filename:"job-output.txt" AND message:"POST-RUN END" AND message:"project-config/playbooks/base/post-ssh")'  # flake8: noqa
     ' OR '
     '(filename:"console.html" AND (message:"[Zuul] Job complete" OR message:"[SCP] Copying console log" OR message:"Grabbing consoleLog"))'  # flake8: noqa
     ' AND build_status:"FAILURE"'
