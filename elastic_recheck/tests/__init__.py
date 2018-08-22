3d0ea8cd30df4e81cd909037ad71a59671105c91
Chuck Short <chucks@redhat.com>
2018-08-21 21:39:49 -0400

    Drop mox usage
    Mox is not being used anywhere, so remove it.
    
    Change-Id: I66bee0fa0d22e99554eae51c74b149d212ede7ed
    Signed-off-by: Chuck Short <chucks@redhat.com>

--- elastic_recheck/tests/__init__.py
+++ elastic_recheck/tests/__init__.py
@@ -15,24 +15,7 @@
 import os
 
 import fixtures
-import mox
-import stubout
 import testtools
-
-
-class MoxStubout(fixtures.Fixture):
-    """Deal with code around mox and stubout as a fixture."""
-
-    def setUp(self):
-        super(MoxStubout, self).setUp()
-        # emulate some of the mox stuff, we can't use the metaclass
-        # because it screws with our generators
-        self.mox = mox.Mox()
-        self.stubs = stubout.StubOutForTesting()
-        self.addCleanup(self.stubs.UnsetAll)
-        self.addCleanup(self.stubs.SmartUnsetAll)
-        self.addCleanup(self.mox.UnsetStubs)
-        self.addCleanup(self.mox.VerifyAll)
 
 
 class TestCase(testtools.TestCase):
@@ -47,7 +30,3 @@ class TestCase(testtools.TestCase):
                 os.environ.get('OS_STDERR_CAPTURE') == '1'):
             stderr = self.useFixture(fixtures.StringStream('stderr')).stream
             self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
-
-        mox_fixture = self.useFixture(MoxStubout())
-        self.mox = mox_fixture.mox
-        self.stubs = mox_fixture.stubs
